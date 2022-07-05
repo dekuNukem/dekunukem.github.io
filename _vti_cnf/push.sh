1f849f81463230ebd6f3cbc9f5e7f181b092ec8b
dekunukem <dekunukem@gmail.com>
2022-07-05 01:26:36 +0100

    test

---
+++ _vti_cnf/push.sh
@@ -0,0 +1,5 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|02 Apr 2022 23:05:40 -0000
+vti_extenderversion:SR|4.0.2.7802
+vti_cacheddtm:TX|02 Apr 2022 23:05:40 -0000
+vti_filesize:IR|481
---
+++ _vti_cnf/reset.sh
@@ -0,0 +1,5 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|04 Jun 2020 09:17:27 -0000
+vti_extenderversion:SR|4.0.2.7802
+vti_cacheddtm:TX|04 Jun 2020 09:17:27 -0000
+vti_filesize:IR|40
